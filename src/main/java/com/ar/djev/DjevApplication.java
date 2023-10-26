7e84086fa595d3b3d06101bf76ff64bf65cb4d55
Franco Maidana <frandjev@gmail.com>
2023-10-26 00:07:09 -0300

    Se crea estructura

---
+++ src/main/java/com/ar/djev/DjevApplication.java
@@ -0,0 +1,13 @@
+package com.ar.djev;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DjevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DjevApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ar/djev/DjevApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ar.djev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DjevApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
